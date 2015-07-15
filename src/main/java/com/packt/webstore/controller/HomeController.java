cca24d04467e5fdd46de65a4d5e1565db8380146
DanielJunior <danieljunior@id.uff.br>
2015-07-14 22:07:31 -0300

    Initial configuration done!

--- src/main/java/com/packt/webstore/controller/HomeController.java
+++ src/main/java/com/packt/webstore/controller/HomeController.java
@@ -18,9 +18,8 @@ public class HomeController {
 
     @RequestMapping("/")
     public String welcome(Model model) {
-        model.addAttribute("greeting", "Welcome	to	Web	Store!");
-        model.addAttribute("tagline", "The	one	and	only	amazing	webstore");
-
+        model.addAttribute("greeting", "Welcome	to Daniel's Web Store!");
+        model.addAttribute("tagline", "The one and only amazing webstore");
         return "welcome";
     }
 }
