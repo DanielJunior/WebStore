0c3e95496a895768b7cda2b3b8be355125f1c4cf
DanielJunior <danieljunior@id.uff.br>
2015-07-20 17:56:53 -0300

    Creating Product in memory repository, to list some products items.

--- src/main/java/com/packt/webstore/controller/ProductController.java
+++ src/main/java/com/packt/webstore/controller/ProductController.java
@@ -6,8 +6,9 @@
 package com.packt.webstore.controller;
 
 import com.packt.webstore.domain.Product;
+import com.packt.webstore.domain.repository.ProductRepository;
 import java.math.BigDecimal;
-import static javafx.beans.binding.Bindings.and;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,14 +20,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ProductController {
 
+    @Autowired
+    private ProductRepository productRepository;
+
     @RequestMapping("/products")
     public String list(Model model) {
-        Product iphone = new Product("P1234", "iPhone	5s", new BigDecimal(500));
-        iphone.setDescription("Apple iPhone 5s smartphone with 4.00-inch 640x1136 display and 8-megapixel rear camera");
-        iphone.setCategory("Smart Phone");
-        iphone.setManufacturer("Apple");
-        iphone.setUnitsInStock(1000);
-        model.addAttribute("product", iphone);
+        model.addAttribute("products", productRepository.getAllProducts());
         return "products";
     }
 }
