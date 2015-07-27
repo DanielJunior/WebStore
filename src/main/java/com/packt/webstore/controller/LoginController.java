3f8a8527e41aa21d0baca834441b28ba9856654a
DanielJunior <danieljunior@id.uff.br>
2015-07-27 17:22:40 -0300

    Adding basic authentication with Spring Security basic features. Now, it's necessary is already logged  to add a new product.

---
+++ src/main/java/com/packt/webstore/controller/LoginController.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.packt.webstore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author danieljr
+ */
+@Controller
+public class LoginController {
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping(value = "/loginfailed", method = RequestMethod.GET)
+    public String loginerror(Model model) {
+
+        model.addAttribute("error", "true");
+        return "login";
+
+    }
+
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public String logout(Model model) {
+        return "login";
+    }
+}
--- src/main/webapp/WEB-INF/views/addProduct.jsp
+++ src/main/webapp/WEB-INF/views/addProduct.jsp
@@ -17,6 +17,7 @@
     <body>
         <section>
             <div class="jumbotron">
+                <a href="<c:url	value="/j_spring_security_logout" />"	class="btn btn-danger btn-mini pull-right">logout</a>		
                 <div class="container">
                     <h1>Products</h1>
                     <p>Add products</p>
