26a0d68892d318c89eacb2e59fbfcc786a9b32df
DanielJunior <danieljunior@id.uff.br>
2015-07-28 15:47:47 -0300

    Showing product images

--- src/main/java/com/packt/webstore/domain/repository/impl/InMemoryProductRepository.java
+++ src/main/java/com/packt/webstore/domain/repository/impl/InMemoryProductRepository.java
@@ -25,19 +25,19 @@ public class InMemoryProductRepository implements ProductRepository {
     private List<Product> listOfProducts = new ArrayList<>();
 
     public InMemoryProductRepository() {
-        Product iphone = new Product("P1234", "iPhone 5s", new BigDecimal(500));
+        Product iphone = new Product("P1", "iPhone 5s", new BigDecimal(500));
         iphone.setDescription("Apple	iPhone 5s smartphone with 4.00-inch 640x1136 display and 8-megapixel rear camera");
         iphone.setCategory("Smart Phone");
         iphone.setManufacturer("Apple");
         iphone.setUnitsInStock(1000);
 
-        Product laptop_dell = new Product("P1235", "Dell Inspiron", new BigDecimal(700));
+        Product laptop_dell = new Product("P2", "Dell Inspiron", new BigDecimal(700));
         laptop_dell.setDescription("Dell Inspiron 14-inch Laptop (Black) with 3rd Generation Intel Core	processors");
         laptop_dell.setCategory("Laptop");
         laptop_dell.setManufacturer("Dell");
         laptop_dell.setUnitsInStock(1000);
 
-        Product tablet_Nexus = new Product("P1236", "Nexus 7", new BigDecimal(300));
+        Product tablet_Nexus = new Product("P3", "Nexus 7", new BigDecimal(300));
         tablet_Nexus.setDescription("Google Nexus 7 is the lightest 7 inch tablet With a quad-core Qualcomm SnapdragonTM S4 Pro processor");
         tablet_Nexus.setCategory("Tablet");
         tablet_Nexus.setManufacturer("Google");
