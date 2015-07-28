6efa90f109c2aa8d77c603010cfc79fb6744ede4
DanielJunior <danieljunior@id.uff.br>
2015-07-28 15:28:31 -0300

    Learning the differences between redirect and forward.

--- src/main/java/com/packt/webstore/controller/HomeController.java
+++ src/main/java/com/packt/webstore/controller/HomeController.java
@@ -20,6 +20,12 @@ public class HomeController {
     public String welcome(Model model) {
         model.addAttribute("greeting", "Welcome	to Daniel's Web Store!");
         model.addAttribute("tagline", "The one and only amazing webstore");
+//        if i want to redirect my request but mantainnig my object modification i have to use 'forward' otherwise use 'redirect' to do new request
+        return "welcome";
+    }
+
+    @RequestMapping("/welcome/greeting")
+    public String greeting() {
         return "welcome";
     }
 }
